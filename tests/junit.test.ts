2d6636cdaed986f46c426622a47a7c61dedda7e8
Tian Feng <tian.feng@saucelabs.com>
2023-06-19 13:01:10 -0700

    do more assertions

--- tests/junit.test.ts
+++ tests/junit.test.ts
@@ -31,6 +31,11 @@ describe('toJUnitObj', function () {
     it('should convert to JUnit object', function() {
         const jObj = run.toJUnitObj()
         expect(jObj.testsuite.length).toBe(2)
+        expect(jObj._status).toBe('failed')
+        expect(jObj._tests).toBe(3)
+        expect(jObj._failures).toBe(1)
+        expect(jObj._skipped).toBe(1)
+        expect(jObj._time).toBe(369)
         expect(jObj.testsuite[0].testcase.length).toBe(0)
         expect(jObj.testsuite[1].testcase.length).toBe(3)
         expect(jObj.testsuite[1].testcase[0]._name).toBe('yay')
