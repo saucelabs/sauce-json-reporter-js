468c8ea3f084cd7fda8f30d78cbceeddcb2de02d
Tian Feng <tian.feng@saucelabs.com>
2023-06-20 14:23:29 -0700

    add test cases for metadata part

--- tests/junit.test.ts
+++ tests/junit.test.ts
@@ -5,6 +5,9 @@ describe('toJUnitObj', function () {
 
     beforeEach(function() {
         const s1 = run.withSuite('somegroup')
+        s1.metadata = {
+            id: 1234
+        }
         const s2 = s1.withSuite('somefile.test.js')
         s2.withTest('yay', {
             status: Status.Passed,
@@ -12,21 +15,28 @@ describe('toJUnitObj', function () {
             attachments: [
               {name: 'video.mp4', path: './video.mp4', contentType: 'video/mp4'},
             ],
-          })
-          s2.withTest('nay', {
+        })
+        s2.withTest('nay', {
             status: Status.Failed,
             output: 'test failed',
             duration: 123,
             attachments: [
               {name: 'video.mp4', path: './video.mp4', contentType: 'video/mp4'},
             ]
-          })
-          s2.withTest('oops', {
+        })
+        s2.withTest('oops', {
             status: Status.Skipped,
             output: 'test skipped',
             duration: 123,
-          })    
-        })
+        })    
+        s2.metadata = {
+            items: {
+              test1: 1,
+              test2: 2,
+            },
+            ids: [1, 2, 3]
+        }
+   })
 
     it('should convert to JUnit object', function() {
         const jObj = run.toJUnitObj()
@@ -37,7 +47,13 @@ describe('toJUnitObj', function () {
         expect(jObj._skipped).toBe(1)
         expect(jObj._time).toBe(369)
         expect(jObj.testsuite[0].testcase.length).toBe(0)
+        expect((jObj.testsuite[0].properties as any).property[0]._name).toBe('id')
+        expect((jObj.testsuite[0].properties as any).property[0]._value).toBe(1234)
         expect(jObj.testsuite[1].testcase.length).toBe(3)
+        expect((jObj.testsuite[1].properties as any).property[0]._name).toBe('items')
+        expect((jObj.testsuite[1].properties as any).property[0]._value).toStrictEqual({"test1": 1, "test2": 2})
+        expect((jObj.testsuite[1].properties as any).property[1]._name).toBe('ids')
+        expect((jObj.testsuite[1].properties as any).property[1]._value).toStrictEqual([1, 2, 3])
         expect(jObj.testsuite[1].testcase[0]._name).toBe('yay')
         expect(jObj.testsuite[1].testcase[0]._time).toBe(123)
         expect(jObj.testsuite[1].testcase[1].failure).not.toBe(undefined)
